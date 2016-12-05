88b86634c7fd60d1ba0afcca4c85cf42340965b8
Fraz Ahsan <fraz.ahsan17@gmail.com>
2016-12-05 16:17:21 +0500

    Donations refresh added

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,14 +2,14 @@ class Donation < ActiveRecord::Base
 	validates_presence_of :amount
 	before_create :set_month
 	after_save :set_funds
-
+	after_destroy :set_funds
 	def set_month
 		self.month = Date.today.strftime("%B %Y")
 	end
 
 	def set_funds
 		f = Fund.find_or_create_by(id: 1)
-		f.total_amount = f.total_amount.to_f + self.amount.to_f
+		f.total_amount = self.amount.to_f
 		f.remaining_amount = f.remaining_amount.to_f + self.amount.to_f
 		f.save
 	end
